75d14bf808cc63646a8bcd277b39c3a84f55b132
James Galantino <james_galantino@aol.com>
2021-04-17 22:11:49 -0400

    Updated seeds, imports for Post model

--- app/models/Post.py
+++ app/models/Post.py
@@ -11,3 +11,4 @@ class Post(Base):
   user_id = Column(Integer, ForeignKey('users.id'))
   created_at = Column(DateTime, default=datetime.now)
   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+  user = relationship('User')
