ab5e217f316d9ece04dac319ed659f40dfac6429
James Galantino <james_galantino@aol.com>
2021-04-17 22:36:55 -0400

    Added ref to Post model for Vote, seeds

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
 
 class Post(Base):
   __tablename__ = 'posts'
@@ -15,3 +16,7 @@ class Post(Base):
   user = relationship('User')
 
   comments = relationship('Comment', cascade='all,delete')
+
+  vote_count = column_property(
+    select([func.count(Vote.id)]).where(Vote.post_id == id)
+  )
