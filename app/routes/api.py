b9410a3502e86b72615bdef5131e29563b7c7eeb
James Galantino <james_galantino@aol.com>
2021-04-21 10:42:10 -0400

    Updated signup route to include user dict

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
 
@@ -7,6 +7,17 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 @bp.route('users', methods=['POST'])
 def signup():
   data = request.get_json()
-  print(data)
+  db = get_db()
 
-  return ''
+  # create a new user
+  newUser = User(
+    username = data['username'],
+    email = data['email'],
+    password = data['password']
+  )
+
+  # save in database
+  db.add(newUser)
+  db.commit()
+
+  return jsonify(id = newUser.id)
