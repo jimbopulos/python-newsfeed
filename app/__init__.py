95b3b42bf37211f1cfecfd1bf7cab48754b5e647
James Galantino <james_galantino@aol.com>
2021-04-30 16:08:31 -0400

    Fixed bug in app init file

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from app.routes import home, dashboard, api
 def create_app(test_config=None):
     # set up app config
     app = Flask(__name__, static_url_path='/')
-    app.url_map.strict_splashes = False
+    app.url_map.strict_slashes = False
     app.config.from_mapping(
       SECRET_KEY='super_secret_key'
     )
